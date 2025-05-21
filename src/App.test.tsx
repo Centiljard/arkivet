50d5571f825d7f0a23055b9ca3c4cf66859e4baf
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-05-21 22:53:23 +0200

    Add links to Skola77, helenelund.org and Texter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,5 @@ import App from './App.tsx'
 describe('App', () => {
   test('renders', () => {
     render(<App />)
-    expect(screen.getByText('Learn React')).toBeDefined()
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import logo from './logo.svg'
-
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div>
+      <div className="flex justify-center items-center mb-50 mt-10">
+        <h1 className="text-7xl font-semibold">Arkivet</h1>
+      </div>
+      <div className="archiveGrid grid grid-cols-3 grid-rows-3 m-auto w-9/10 gap-10">
+        <div className="aspect-2/1 w-full rounded-xl bg-green-500">
+          <h2 className="skola77 text-4xl text-white font-semibold text-center">
+            Skola77
+          </h2>
+        </div>
+        <div className="helenelundDotOrg aspect-2/1 full rounded-xl bg-[#f2f2f2]">
+          <h2 className="text-4xl font-semibold text-center">Helenelund.org</h2>
+        </div>
+        <div className='texter bg-black aspect-2/1 w-full rounded-xl'>
+          <h2 className="text-4xl font-semibold text-white text-center">Texter</h2>
+        </div>
+      </div>
     </div>
   )
 }
